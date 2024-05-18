b7f6e902ce3c2668481bb04207f7776544022e7a
arkalsekar <abdulrehmankalsekar10@gmail.com>
2024-05-18 18:20:37 +0530

    Added Files

---
+++ 02_IO.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        
+        // Output
+        System.out.println("Hello world !!");
+        
+        // Input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enteer a Number");
+        int n = scan.nextInt();
+        System.out.println("The Number entered is " + n);
+    }
+}
