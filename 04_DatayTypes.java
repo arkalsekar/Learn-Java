5b64100983d6aa22c12f59ab01b532fb0bbda637
arkalsekar <abdulrehmankalsekar10@gmail.com>
2024-06-01 18:46:03 +0530

    Added Codes

---
+++ 04_DatayTypes.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Abdul Rehman";
+        String username = "arkalsekar";
+        char div = 'D';
+        int age = 20;
+        float years = 19.5f;
+        boolean isDev = true;
+
+        System.out.println(name);
+        System.out.println(username);
+        System.out.println(div);
+        System.out.println(age);
+        System.out.println(years);
+    }
+}
---
+++ 07_Operators.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+
+        // Arithematic Operators
+        System.out.println("Addition " + (a + b));
+        System.out.println("Subtraction " + (a - b));
+        System.out.println("Multiplication " + (a * b));
+        System.out.println("Division " + (a / b));
+        System.out.println("Modulus " + (a % b));
+
+        // Relational Operator 
+        System.out.println("Greater Than " + (a > b));
+        System.out.println("Less Than " + (a < b));
+        System.out.println("Greater Than or Equal to " + (a >= b));
+        System.out.println("Less Than or Equal to " + (a <= b));
+        System.out.println("is Equal to " + (a == b));
+        System.out.println("is not Equal to " + (a != b));
+
+        // Assignment Operator
+        int c = 23;
+
+        // Compound Assignment Operator 
+        System.out.println("Pre Increment " + ++a);
+        System.out.println("Post Decrement " + a++);
+        System.out.println("Pre Increment " + --a);
+        System.out.println("Post Decrement " + a--);
+        // += -= *= /= 
+        
+        // Logical Operator
+        // Logical And &&
+        // Logical Or ||
+        // Logical Not !
+    }
+}
